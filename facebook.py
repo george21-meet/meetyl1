0a166a610d53fb13a98f5d611d601951fdaedf48
george21-meet <52972058+george21-meet@users.noreply.github.com>
2019-11-24 19:42:30 +0200

    Add files via upload

--- facebook.py
+++ facebook.py
@@ -1,7 +1,13 @@
 import sqlalchemy
-Base=declarative_base()
+#from model import Base, Student
+#from sqlalchemy import create_engine
+#from sqlalchemy.orm import  sess
+#Base=declarative_base()
 userlist=[]
 passwordlist=[]
+posts=[]
+comments=[]
+posts_list=[]
 class post():
 	def __init__(self,text,list4,name):
 		self.text=text
@@ -9,16 +15,18 @@ class post():
 		self.name=name
 		self.list.append(self.text)
 		print(self.name,"has posted",self.text)
-class User(Base):
+		posts_list.append(self.text)
+class User():
 	def __init__(self,name,email,password):
-		__tablename__='datatable'
-		
+		#__tablename__='datatable'
+		#name=column(String,primary_key=True)
+		#email=column(email)
+		#password=column(password)
 		self.name=name
 		self.email=email
 		self.password=password
 		self.friends_list=[]
-		self.posts=[]
-		userlist.append(self)
+		userlist.append(self.name)
 		passwordlist.append(self.password)
 	def addfriend(self,email1,friends_list):
 		self.email1=email1
@@ -33,7 +41,7 @@ class User(Base):
 		print("email:",self.email)
 		print("password:",self.password)
 		print("friends list:",self.friends_list)
-		print("posts:",self.posts)
+		print("posts:",posts)
 	def addmessage(self,message):
 		self.message=message
 		messagelist=[]
@@ -49,20 +57,37 @@ class User(Base):
 	def addusertolist(self):
 		userlist.append(self.User)
 		password.append(self.password)
+	def comment(self,post3,comment1):
+		self.post=post3
+		self.comment1=comment1
+		print(self.name,"has commented on",self.post,self.comment1)
+		comments.append(self.comment1)
+	def like(self,post4,):
+		self.post1=post4
+		print(self.name,"has liked",self.post)
 
 user1=User("loai","loai17@meet.mit.edu","myhiddenpassword123")
 user2=User("anas","aabbassi@js-bethlehem.com","123456")
 user1.addfriend(user2.email,user1.friends_list)
-user1.post1("hi my name is loai",user1.posts)
+user1.post1("hi my name is loai",posts)
 user1.get_userinfo()
 user1.removefriend(user2.email,user1.friends_list)
 user1.addmessage("hello i am mark")
 user2.recievemessage(user1)
+user1.comment(posts[0],"hi loai")
+user1.like(posts[0])
+print(userlist,passwordlist)
 print("enter ur username")
+f=open('facebookbase.txt','a')
+f.write(userlist)
+f.close()
+
 a=input()
 if a in userlist:
 	print("enter ur password")
-	a=input()
-	if a in passwordlist:
+	b=input()
+	if b in passwordlist:
 		print("u got in")
 		print(user1)
+else:
+	print("username not in")
